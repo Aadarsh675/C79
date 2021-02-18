6f9b12713708cc892d15f6b0961073f7ca283a3e
Aadarsh675 <76542875+Aadarsh675@users.noreply.github.com>
2021-02-17 20:22:18 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var menu_list_array = ["Chicken Tandoori Pizza", "Veg Supreme Pizza", "Paneer Tikka Pizza", "Deluxe Veggie Pizza", "Veg Extravanganza Pizza"];
+function getmenu(){
+    var htmldata;
+    htmldata = "<o1 class = 'menulist'>"
+    menu_list_array.sort();
+    for (var i = 0; i < menu_list_array.length; i++){
+        htmldata = htmldata + "<l1>" + menu_list_array[i] + "</li>"
+    }
+    htmldata = htmldata + "</o1>"
+    document.getElementById("display_menu").innerHTML = htmldata;
+}
+function additem(){
+    var htmldata;
+    var item = document.getElementById("add_item").value;
+    menu_list_array.push("item");
+    menu_list_array.sort();
+    htmldata = "<section class = 'cards'>"
+    for (var i = 0; i < menu_list_array.length; i++){
+        htmldata = htmldata + "<div class = 'card'>" + "<img src = 'images/pizzaImg.png'/>" + menu_list_array[i] + "</div>";
+    }
+    htmldata = htmldata + "</section";
+    document.getElementById("display_addedmenu").innerHTML = htmldata;
+}
